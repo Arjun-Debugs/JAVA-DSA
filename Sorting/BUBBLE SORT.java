afafaf8473c45f4ecac1bcd224cd153ecaa39117
Arjun Sudheer <arjunsudheer399@gmail.com>
2025-06-14 09:41:14 +0530

    Create BUBBLE SORT.java

---
+++ Sorting/BUBBLE SORT.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        for (int nums : arr) {
+            System.out.print(nums + " ");
+        }
+    }
+}
